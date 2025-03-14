7e743c45bf1ff30b275523e636e8c7305ea5b1dc
diyasini13 <diyasini@google.com>
2025-03-14 11:23:12 +0530

    Changes to auth

--- app.py
+++ app.py
@@ -3,14 +3,49 @@ from google.cloud import vision
 import os
 from PIL import Image , ImageDraw
 import io
-import auth_token
 import json
+import streamlit as st
+import jwt
+import os
+from dotenv import load_dotenv
 
 
 # Set your Google Cloud credentials (replace with your actual path)
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/credentials.json"
 
+load_dotenv()
 
+def authentication():
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+
+    def decode_token(token):
+        try:
+            # Decode the token with the same secret key
+            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            return payload
+        except jwt.ExpiredSignatureError:
+            return "Expired"
+        except jwt.InvalidTokenError:
+            return "Invalid"
+
+    # Get the token from query parameters
+    query_params = st.query_params
+    token = query_params.get("token", None)
+
+    if not token:
+        st.error("Unauthorized access. Token not provided.")
+        st.stop()
+
+    # Decode the token
+    decoded = decode_token(token)
+
+    if decoded == "Expired":
+        st.error("Token has expired. Please login again.")
+        st.stop()
+
+    elif decoded == "Invalid":
+        st.error("Invalid token. Access denied.")
+        st.stop()
 
 def detect_labels(image_content):
     """Detects labels in the provided image content."""
@@ -128,5 +163,5 @@ def app():
                         st.json(results)
 
 if __name__ == "__main__":
-    auth_token.authentication();
+    authentication();
     app();
